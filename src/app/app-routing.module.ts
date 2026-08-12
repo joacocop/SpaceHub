76fbefbed8b73fd4b4682406d3d09cd13832d5c6
joacocop <joaquiinacevedo@gmail.com>
2026-08-12 20:29:21 -0300

    s4 y s5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MetricsComponent } from './components/metrics/metrics.component';
+import { TrajectoryComponent } from './components/trajectory/trajectory.component';
+import { AstronautsComponent } from './components/astronauts/astronauts.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LaunchHistoryComponent } from './components/launch-history/launch-history.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'metrics', component: MetricsComponent },
+  { path: 'trajectory', component: TrajectoryComponent },
+  { path: 'astronauts', component: AstronautsComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'launch-history', component: LaunchHistoryComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MetricsComponent } from './components/metrics/metrics.component';
+import { TrajectoryComponent } from './components/trajectory/trajectory.component';
+import { AstronautsComponent } from './components/astronauts/astronauts.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LaunchHistoryComponent } from './components/launch-history/launch-history.component';
 import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
@@ -16,10 +21,15 @@ import { FilterPipe } from './pipes/filter.pipe';
     HomeComponent,
     DashboardComponent,
     MetricsComponent,
+    TrajectoryComponent,
+    AstronautsComponent,
+    ChatComponent,
+    LaunchHistoryComponent,
     FilterPipe
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     HttpClientModule,
     FormsModule,
     AppRoutingModule
